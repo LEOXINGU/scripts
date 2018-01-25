e72efdf8c3288a45ed57530bd76add537e4d47e8
Leandro França <geoleandro.franca@gmail.com>
2018-01-25 08:46:46 -0300

    bug fixed

--- RESTAURAR.py
+++ RESTAURAR.py
@@ -61,7 +61,7 @@ if os.path.isdir(pasta):
     os.chdir(pasta)
     sentinela = True
 else:
-    pasta = 'C:\Program Files (x86)/PostgreSQL/'+version+'/bin'
+    pasta = 'C:/Program Files (x86)/PostgreSQL/'+version+'/bin'
     if os.path.isdir(pasta):
         os.chdir(pasta)
         sentinela = True
