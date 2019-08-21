a1148c90b7119a486ed3cb243f7e601481889ce7
Leandro França <geoleandro.franca@gmail.com>
2019-08-20 21:56:45 -0300

    new tool
    Ferramenta para carregar raster no BDGeo PostGIS

--- tiff2PostGIS.py
+++ tiff2PostGIS.py
@@ -2,8 +2,8 @@
 /***************************************************************************
  LEOXINGU
                               -------------------
-        begin                : 2017-09-25
-        copyright            : (C) 2017 by Leandro Franca - Cartographic Engineer
+        begin                : 2019-08-19
+        copyright            : (C) 2019 by Leandro Franca - Cartographic Engineer
         email                : geoleandro.franca@gmail.com
  ***************************************************************************/
 
@@ -15,18 +15,18 @@
  *                                                                         *
  ***************************************************************************/
 """
-# CLONAR BD
-##4. CLONAR BD=name
+# CARREGAR RASTER
+##7. CARREGAR RASTER=name
 ##LF01) PostGIS=group
-##Nome_do_BD_original=string
-##Nome_do_BD_clonado=string
-##Host=string localhost
+##Raster=raster
+##database=string BDGeo
+##schema=string public
+##table=string tabela
 ##Versao_do_PostgreSQL=selection 10;9.3;9.4;9.5;9.6
 ##Usuario=string postgres
+##Host=string localhost
 
 # Inputs
-template = str(Nome_do_BD_original)
-novo = str(Nome_do_BD_clonado)
 host = str(Host)
 lista = [10,9.3,9.4,9.5,9.6]
 version = str(lista[Versao_do_PostgreSQL])
@@ -37,6 +37,15 @@ from qgis.gui import QgsMessageBar
 from qgis.utils import iface
 from qgis.core import *
 import time
+import gdal
+from osgeo import osr
+
+# Pegar SRC do Raster de entrada
+image = gdal.Open(Raster)
+prj=image.GetProjection()
+crs = QgsCoordinateReferenceSystem()
+crs.createFromWkt(prj)
+SRID = (crs.authid()).split(':')[-1]
 
 local = 'C:/Program Files/PostgreSQL/'+version+'/bin'
 sentinela = False
@@ -55,21 +64,27 @@ else:
         iface.messageBar().pushMessage(u'Erro', "Problema(s) durante a execucao do comando.", level=QgsMessageBar.CRITICAL, duration=5) 
 
 if sentinela:
-    arquivo = open('C:/Users/Public/muda.sql','w')
-    for item in novo.split(','):
-        arquivo.write('CREATE DATABASE '+item.replace('-', '_')+' WITH TEMPLATE '+template+';\n')
-    arquivo.close()
-    comando = 'psql -d postgres -U '+Usuario+' -h '+host+' -p 5432 -f C:/Users/Public/muda.sql'
-    progress.setInfo('<b>Clonando o Banco de Dados...</b><br/>')
+    arqSQL = 'C:/Users/Public/raster2postgis.sql'
+    comando = 'raster2pgsql -s ' + SRID + ' -I -C -M ' + Raster + ' -F -t 100x100 ' +schema+ '.'+ table + ' > '+ arqSQL
+    progress.setInfo('<b>Raster para SQL...</b><br/>')
     result = os.system(comando)
-    os.remove('C:/Users/Public/muda.sql')
+    os.remove(arqSQL)
     if result==0:
-        progress.setInfo('<b>Operacao concluida com sucesso!</b><br/><br/>')
-        progress.setInfo('<b>Leandro Fran&ccedil;a - Eng Cart</b><br/>')
-        time.sleep(4)
-        iface.messageBar().pushMessage(u'Situacao', "Operacao Concluida com Sucesso!", level=QgsMessageBar.INFO, duration=5) 
+        progress.setInfo('<b>Carregando raster no BDGeo...</b><br/>')
+        comando2 = 'psql -d ' +database+' -U '+Usuario+' -h '+host+' -p 5432 -f ' + arqSQL
+        result2 = os.system(comando2)
+        if result2 == 0:
+            progress.setInfo('<b>Operacao concluida com sucesso!</b><br/><br/>')
+            progress.setInfo('<b>Leandro Fran&ccedil;a - Eng Cart</b><br/>')
+            time.sleep(4)
+            iface.messageBar().pushMessage(u'Situacao', "Operacao Concluida com Sucesso!", level=QgsMessageBar.INFO, duration=5) 
+        else:
+            progress.setInfo('<b>Problema(s) durante a execucao da operacao.</b><br/>')
+            progress.setInfo('<b>Verifique se os parametros foram definidos corretamente.</b><br/>')
+            time.sleep(8)
+            iface.messageBar().pushMessage(u'Erro', "Problema(s) durante a execucao do comando.", level=QgsMessageBar.CRITICAL, duration=10) 
     else:
-        progress.setInfo('<b>Problema(s) durante a execucao do backup.</b><br/>')
+        progress.setInfo('<b>Problema(s) durante a execucao da operacao.</b><br/>')
         progress.setInfo('<b>Verifique se os parametros foram definidos corretamente.</b><br/>')
         time.sleep(8)
         iface.messageBar().pushMessage(u'Erro', "Problema(s) durante a execucao do comando.", level=QgsMessageBar.CRITICAL, duration=10) 
